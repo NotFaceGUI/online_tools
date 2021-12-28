51db9a49b3b74bc218a3975c86a5640e5d8c7aae
NotFaceGUI <2509191792@qq.com>
2021-12-28 20:07:24 +0800

    del

--- src/router/index.js
+++ src/router/index.js
@@ -12,10 +12,7 @@ const routes = [{
   {
     path: '/login',
     name: 'Login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import( /* webpackChunkName: "about" */ '../views/Login.vue')
+    component: () => import('../views/Login.vue')
   },
   {
     path: '/user/:name',
